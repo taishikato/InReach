49f51feaaf8624d3dff48d05d68494798d58985d
taishikato <taishi.k0903@gmail.com>
2024-12-06 22:41:46 +0900

    Add OpenAI function integration in Supabase setup

---
+++ supabase/functions/openai/index.ts
@@ -0,0 +1,50 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+import { OpenAI } from "https://deno.land/x/openai@v4.24.0/mod.ts";
+import process from "node:process";
+// import { OpenAI } from "npm:openai@4.76.0";
+// Setup type definitions for built-in Supabase Runtime APIs
+/// <reference types="https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts" />
+
+console.log("Hello from Functions!");
+
+Deno.serve(async (req) => {
+  // const { apiKey } = await req.json();
+  // const data = {
+  //   message: `Hello ${name}!`,
+  // };
+
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "user", content: "hello" }],
+    model: "gpt-3.5-turbo",
+  });
+
+  return new Response(
+    JSON.stringify({ text: completion.choices[0].message.content }),
+    {
+      headers: { "Content-Type": "application/json" },
+    },
+  );
+
+  // return new Response(
+  //   JSON.stringify(data),
+  //   { headers: { "Content-Type": "application/json" } },
+  // );
+});
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/openai' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
