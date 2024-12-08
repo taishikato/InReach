ad7293eb402bf11a4a33088a2b9e18973c90173c
taishikato <taishi.k0903@gmail.com>
2024-12-08 11:14:53 +0900

    add posthog proxy

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,8 +5,10 @@ import { PostHogProvider } from "posthog-js/react";
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    // api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    api_host: "/ingest",
+    ui_host: "https://us.posthog.com",
   });
 }
 
