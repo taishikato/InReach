5a3993f4405279ec68d3ea3abc3d979aa6002ad4
taishikato <taishi.k0903@gmail.com>
2024-12-08 11:18:29 +0900

    Initialize PostHog only in production environment

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,14 +4,19 @@ import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
 if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    // api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
-    api_host: "/ingest",
-    ui_host: "https://us.posthog.com",
-  });
+  if (process.env.NODE_ENV === "production") {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+      api_host: "/ingest",
+      ui_host: "https://us.posthog.com",
+    });
+  }
 }
 
 export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+  if (process.env.NODE_ENV === "production") {
+    return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+  }
+
+  return <>{children}</>;
 }
